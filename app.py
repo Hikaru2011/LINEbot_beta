c0937d1398ae185f85ad75d0ca4070a6c4f86a64
Hikaru2011 <hikaruimamura0516@gmail.com>
2025-06-17 19:05:22 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from inspect import signature
+from tkinter import Image
 from flask import Flask , request , abort
 
 from linebot import LineBotApi , WebhookHandler
 from linebot.exceptions import InvalidSignatureError
-from linebot.models import MessageEvent, TextMessage, TextSendMessage
+from linebot.models import MessageEvent, TextMessage, ImageMessage
 
 import os
 
@@ -15,6 +16,11 @@ import gspread
 from gspread_dataframe import get_as_dataframe, set_with_dataframe
 from oauth2client.service_account import ServiceAccountCredentials
 
+from googleapiclient.discovery import build
+from googleapiclient.http import MediaFileUpload
+from oauth2client.service_account import ServiceAccountCredentials
+
+
 app = Flask(__name__)
 
 #通信するためのキーを取得。
@@ -25,7 +31,7 @@ LINE_CHANNEL_SECRET = os.getenv("LINE_CHANNEL_SECRET")
 line_bot_api = LineBotApi(LINE_CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(LINE_CHANNEL_SECRET)
 
-def append_excel(message_text, timestamp_str):
+def append_excel(user_name,message_text, timestamp_str):
 
     #許可できる動作を定義
     scope = [
@@ -46,15 +52,57 @@ def append_excel(message_text, timestamp_str):
     try:
         df = get_as_dataframe(sheet).dropna(how="all")
     except:
-        df = pd.DataFrame(columns=["投稿時間","メッセージ"])
+        df = pd.DataFrame(columns=["投稿者","投稿時間","メッセージ"])
 
     
-    new = {"投稿時間":timestamp_str,"メッセージ":message_text}
+    new = {"投稿者":user_name,"投稿時間":timestamp_str,"メッセージ":message_text}
 
     df = pd.concat([df,pd.DataFrame([new])], ignore_index=True) #つなげる((もとのdf,新しく作ったdf(newの1行),indexは振りなおす)
 
     sheet.clear()
     set_with_dataframe(sheet, df)
+
+def upload_to_drive(file_path,file_name):
+
+    #許可できる動作を定義
+    scope = [
+  "https://www.googleapis.com/auth/drive"
+  ]
+    
+    #認証情報を取得
+    creds = ServiceAccountCredentials.from_json_keyfile_name("formal-analyzer-463112-c6-e394ee3ce1d7.json", scope)
+    
+    #認証→通信のためのものを格納。
+    service = build("drive","v3",credentials=creds)
+
+    file_metadata = {
+        "name":file_name,
+        "parents":["1fUL7eyb4WpHIXZSLGLR49PYLwgpkASZg"]
+    }
+
+    media = MediaFileUpload(file_path,miethtype=("image/jpeg"))
+
+    uploaded_file = service.files().create(
+        body=file_metadata,
+        media_dody=media,
+        fileds="id"
+    ).execute()
+
+    service.permissions().create(
+        fileId=uploaded_file["id"]
+        body={"rore":"reder","type":"anyone"}
+    ).execute()
+
+    file_url = f"https://drive.google.comuc?id={uploaded_file["id"]}"
+
+    return file_url
+
+#名前取得の関数
+def getname(event):
+    id = event.source.user_id
+    profile = line_bot_api.get_profile(id)
+    user_name = profile.display_name
+    return user_name
 
 @app.route("/callback", methods=["POST"]) #(@は、この時,,,)/callbackサーバーにリクエストが来た(POST)ら関数を作動。
 def callback():
@@ -69,16 +117,38 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage) #LINEに送られてきて、テキストだったとき関数を作動
 def handle_message(event):
+    user_name = getname(event)
     user_message = event.message.text
     timestamp = event.timestamp / 1000 #ミリ秒を秒に
     dt = datetime.fromtimestamp(timestamp) #それを人間が見れる形に
-
     dt_str = dt.strftime("%Y-%m-%d %H:%M:%S")
 
     print(f"Received message: {user_message} at {dt_str}")
 
-    append_excel(user_message,dt_str)  
+    append_excel(user_name,user_message,dt_str)
+
+@handler.add(MessageEvent, message=ImageMessage)
+def handle_message(event):
+    user_name = getname(event)
+
+    timestamp = event.timestamp / 1000 #ミリ秒を秒に
+    dt = datetime.fromtimestamp(timestamp) #それを人間が見れる形に
+    dt_str = dt.strftime("%Y-%m-%d %H:%M:%S")
+
+    image_id = event.message.id
+    message_contect = line_bot_api.get_message_content(image_id)
+
+    file_name = f"{user_name}_{timestamp}.jpg"
+    file_path = f"/tmp/{file_name}"
+
+    with open(file_path, "wd") as f:
+        for chunk in message_contect.iter_content():
+            f.write(chunk)
+
+    file_url = upload_to_drive(file_path, file_name)
     
+    append_excel(user_name,file_url, timestamp)
+
 @app.route("/")
 def index():
     return "Hello! This is the root page."
